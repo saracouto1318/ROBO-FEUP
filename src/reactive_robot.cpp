8a9398c45af4fd9c012f2ba5e8f38513435fe060
saracouto1318 <saracouto1318@hotmail.com>
2018-10-24 08:36:30 +0100

    change files

--- src/reactive_robot.cpp
+++ src/reactive_robot.cpp
@@ -11,7 +11,7 @@ namespace reactive_robot
 		if(argc != 3)
 		{
 			ROS_ERROR(
-			"Usage : reactive_robot <robot_id> <laser_id>");
+			"Usage : reactive_robot robot <robot_id> <laser_id>");
 			exit(0);
 		}
 
@@ -33,58 +33,45 @@ namespace reactive_robot
 
       float distanceRightSide = numeric_limits<float>::max();
       float distanceLeftSide = numeric_limits<float>::max();
-      float distanceFront = numeric_limits<float>::max();
-      float robotAngle = 0.0;
+			float  distanceFront = numeric_limits<float>::max();
+			float robotAngle = 0.0;
 
     	for(int i = 0; i < scan_.ranges.size(); i++){
 			     float realDistance = scan_.ranges[i];
-			     float sensorAngle = scan_.angle_min + (scan_.angle_increment * (180/PI)) * i;
+			     float sensorAngle = (scan_.angle_min * (180/PI)) + (scan_.angle_increment * (180/PI)) * i;
 
-           if(sensorAngle >= -120 && sensorAngle <= -30){
+           if(sensorAngle >= -120 && sensorAngle <= 0){
    			       if(realDistance < distanceRightSide){
                    robotAngle = sensorAngle;
    					       distanceRightSide = realDistance;
    				     }
      			  }
-     			  else if(sensorAngle > 30 && sensorAngle <= 120){
+     			  else if(sensorAngle > 0 && sensorAngle <= 120){
               if(realDistance < distanceLeftSide){
                     robotAngle = sensorAngle;
                     distanceLeftSide = realDistance;
               }
             }
-            else if(sensorAngle >= -30 && sensorAngle <= 30){
-  			       if(realDistance < distanceFront)
-  					       distanceFront = realDistance;
-      			}
-     	  }
+				}
 
      		float minDistance = min(distanceRightSide, distanceLeftSide);
-     		//float alpha = 90.0 - abs(robotAngle);
+     		float alpha = 90.0 - abs(robotAngle);
 
-     		if(minDistance <= scan_.range_max){
+				cout << "Min Distance: " << minDistance << " Scan Range: " << scan_.range_max << endl;
+
+				if(minDistance <= scan_.range_max){
           cout << "******** WALKING *********" << endl;
      			cout << "DISTANCE TO WALL: " << minDistance << endl;
-     			//cout << "ALPHA: " << alpha << endl;
+     			cout << "ALPHA: " << alpha << endl;
 
      			cmd.linear.x = 0.3;
      			//https://www.seas.upenn.edu/sunfest/docs/papers/12-bayer.pdf
-     			cmd.angular.z = 0.4;
-          //(-20 * (sin((alpha * (PI/180)) - (minDistance - 1.5))) * cmd.linear.x;
+     			cmd.angular.z = (-20 * (sin(alpha * (PI/180)) - (minDistance - 1.5))) * cmd.linear.x;
      		}
      		else{
      			cmd.linear.x = 0.3;
      			cmd.angular.z = 0.1;
      		}
-        /*
-     		if(distanceFront <= 1.5 && distanceRightSide < scan_.range_max && distanceLeftSide < scan_.range_max) // on the pipe
-     		{
-     			cout << "******** STOPPED *********" << endl;
-     			cout << "FRONT DISTANCE: " << distanceFront << endl;
-     			cout << "LEFT DISTANCE: " << distanceLeftSide << endl;
-     			cout << "RIGHT DISTANCE: " << distanceRightSide << endl;
-     			cmd.linear.x = 0.0;
-     			cmd.angular.z = 0.0;
-     		}*/
 
   		cmd_vel_pub_.publish(cmd);
   	}
