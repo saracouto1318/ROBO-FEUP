05f7405ec0d2c9fae90582503328f48839e1ba47
saracouto1318 <saracouto1318@hotmail.com>
2018-11-04 09:22:55 +0000

    clean code

--- src/reactive_robot.cpp
+++ src/reactive_robot.cpp
@@ -46,8 +46,6 @@ void WallFollowing::callback(const sensor_msgs::LaserScan &msg)
 {
 	scan_ = msg;
 	geometry_msgs::Twist cmd;
-	double startTime = time(0);
-	//string filename = string("logs-") + startTime + "txt";
 	char filename[] = "logs.txt";
 
 	float distanceRightSide = numeric_limits<float>::max();
@@ -80,19 +78,11 @@ void WallFollowing::callback(const sensor_msgs::LaserScan &msg)
 
 	float minDistance = min(distanceRightSide, distanceLeftSide);
 
-	/**const float MAX_SPEED = 6.5;
-	const float MIN_SPEED = 1.0;*/
 	const float GOAL_DISTANCE = 1.5;
-	const float K = -15;
-
-	// factor up to 1 to mod speed according to wall distance, where 1 is perfect conditions
-	//float distanceFactor = min(minDistance, GOAL_DISTANCE)/GOAL_DISTANCE;
+	const float K = 15;
 
 	// values for angular speed
 	float alpha = 90.0 - abs(robotAngle);
-	
-
-	cout << "Min Distance: " << minDistance << " Scan Range: " << scan_.range_max << endl;
 
 	if (minDistance <= scan_.range_max)
 	{
@@ -100,25 +90,19 @@ void WallFollowing::callback(const sensor_msgs::LaserScan &msg)
 		cout << "DISTANCE TO WALL: " << minDistance << endl;
 		cout << "ALPHA: " << alpha << endl;
 
-		/*cout << "Distance factor: " << distanceFactor << endl;
-		cmd.linear.x = max(distanceFactor*MAX_SPEED, MIN_SPEED);*/
 		cmd.linear.x = 0.5;
 
 		//https://www.seas.upenn.edu/sunfest/docs/papers/12-bayer.pdf
-		cmd.angular.z = (K * (sin(alpha * (PI / 180)) - (minDistance - GOAL_DISTANCE))) * cmd.linear.x;
+		cmd.angular.z = (-K * (sin(alpha * (PI / 180)) - (minDistance - GOAL_DISTANCE))) * cmd.linear.x;
 	}
 	else
 	{
-		//cmd.linear.x = MAX_SPEED;
 		cmd.linear.x = 0.5;
 		cmd.angular.z = 0.0;
 	}
 
 	cmd_vel_pub_.publish(cmd);
 
-	double timeDiff = time(0) - startTime;
-	char timeDiffStr[50];
-	snprintf(timeDiffStr, 50, "%f", timeDiff);
 	char minDistanceStr[20];
 	snprintf(minDistanceStr, 20, "%f", minDistance);
 	char alphaStr[20];
@@ -129,9 +113,7 @@ void WallFollowing::callback(const sensor_msgs::LaserScan &msg)
 	snprintf(angularStr, 20, "%f", cmd.angular.z);
 
 	char log[400];
-	strcpy(log, "=================\ntime = ");
-	strcat(log, timeDiffStr);
-	strcat(log, "\nwallDistance = ");
+	strcpy(log, "=================\nwallDistance = ");
 	strcat(log, minDistanceStr);
 	strcat(log, "\nalpha = ");
 	strcat(log, alphaStr);
